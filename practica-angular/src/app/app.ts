3ae33fd96cea137bb0831d721602a30b546781b4
ALEK21DS <brandonalexpesantez@gmail.com>
2025-11-21 07:40:54 -0500

    Crear un nuevo componente mi-perfil

--- practica-angular/src/app/app.ts
+++ practica-angular/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Perfil } from "./perfil/perfil";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Perfil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Bienvenido, este es mi nuevo proyecto de practica');
+  protected readonly title = signal('Primer Proyecto Angular');
 }

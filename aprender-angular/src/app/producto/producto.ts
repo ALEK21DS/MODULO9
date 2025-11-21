afea2539725325f27013822e7848ea13da4873d4
ALEK21DS <brandonalexpesantez@gmail.com>
2025-11-21 08:33:46 -0500

    Crear un nuevo constructor

--- aprender-angular/src/app/producto/producto.ts
+++ aprender-angular/src/app/producto/producto.ts
@@ -8,5 +8,8 @@ import { Component } from "@angular/core";
 })
 
 export class Producto{
-
+    constructor(){
+        console.log('Componente Producto Cargado');
+        alert('Hola');
+    }
 }
--- practica-angular/src/app/perfil/perfil.ts
+++ practica-angular/src/app/perfil/perfil.ts
@@ -8,5 +8,8 @@ import { Component } from "@angular/core";
 })
 
 export class Perfil{
-
+    constructor(){
+        alert('¡Bienvenido al perfil, estás dentro de la aplicación!');
+        console.log('Se cargó el componente de perfil correctamente.');
+    }
 }

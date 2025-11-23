2d830a9e251abe5e28f8d7f8043fe8d791373b10
ALEK21DS <brandonalexpesantez@gmail.com>
2025-11-23 16:01:39 -0500

    Implementando del bucle for con Angular

--- aprender-angular/src/app/usuario/usuario.ts
+++ aprender-angular/src/app/usuario/usuario.ts
@@ -9,6 +9,17 @@ import { Component } from '@angular/core';
 export class Usuario {
   web = 'https://amazon.com';
   redirigir = true;
+  marcasAutos = [
+    { id: 1, nombre: 'Toyota' },
+    { id: 2, nombre: 'Ford' },
+    { id: 3, nombre: 'Chevrolet' },
+    { id: 4, nombre: 'Honda' },
+    { id: 5, nombre: 'Nissan' }
+  ];
+
+  /*ngOnInit() {
+    this.marcasAutos = [];
+  }; Para poner la lista vacia*/
 
   cambioRedireccion(){
     if(this.redirigir == false){

e414766c8c40d52ed66b935acd6786370fc86139
ALEK21DS <brandonalexpesantez@gmail.com>
2025-11-23 16:34:29 -0500

    Practica del bucle for con Angular

--- practica-angular/src/app/prestamos/prestamos.ts
+++ practica-angular/src/app/prestamos/prestamos.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-prestamos',
-  imports: [],
   templateUrl: './prestamos.html',
   styleUrl: './prestamos.css',
 })
 export class Prestamos {
   web = 'https://angular.dev';
   redirigir = true;
+  prestamosList = [
+    { id: 1, monto: 5000, estado: 'Aprobado' },
+    { id: 2, monto: 10000, estado: 'Pendiente' },
+    { id: 3, monto: 7500, estado: 'Aprobado' },
+    { id: 4, monto: 20000, estado: 'Aprobado' },
+    { id: 5, monto: 15000, estado: 'Pendiente' }
+  ]
 
   cambioRedireccion(){
     if(this.redirigir == false){
@@ -17,5 +23,4 @@ export class Prestamos {
       this.redirigir = false;
     }
   }
-
 }

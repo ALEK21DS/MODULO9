167b2dbecfb59b7e3032f8bb757a21b184afa368
ALEK21DS <brandonalexpesantez@gmail.com>
2025-11-24 09:20:58 -0500

    Practica con tuberias (Pipes) en Angular

--- practica-angular/src/app/prestamos/prestamos.ts
+++ practica-angular/src/app/prestamos/prestamos.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Transacciones } from '../transacciones/transacciones';
+import { UpperCasePipe, LowerCasePipe, DatePipe, TitleCasePipe, CurrencyPipe, PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'app-prestamos',
-  imports: [Transacciones],
+  imports: [Transacciones, UpperCasePipe, LowerCasePipe, DatePipe, TitleCasePipe, CurrencyPipe, PercentPipe],
   templateUrl: './prestamos.html',
   styleUrl: './prestamos.css',
 })
@@ -20,8 +21,10 @@ export class Prestamos {
 
   prestamo = {
     id: 1,
-    tipo: 'default',
-    interes: 5.5
+    tipo: 'hipotecario',
+    monto: 250000,
+    interes: 0.05,
+    fechaPago: '2025-12-31',
   }
 
   cambioRedireccion(){
